696103e602a6cc0c7577930cbfe4a798116b9b5d
Jakub Kovář <6195502+minop@users.noreply.github.com>
2025-09-24 10:55:42 +0200

    clean-up

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -5,7 +5,7 @@ import {SvgService} from '../../services/svg.service';
 import {Diagram} from '../../classes/diagram/diagram';
 import {ExampleFileComponent} from "../example-file/example-file.component";
 import {FileReaderService} from "../../services/file-reader.service";
-import { HttpClient } from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'app-display',
@@ -28,7 +28,7 @@ export class DisplayComponent implements OnDestroy {
 
         this.fileContent = new EventEmitter<string>();
 
-        this._sub  = this._displayService.diagram$.subscribe(diagram => {
+        this._sub = this._displayService.diagram$.subscribe(diagram => {
             console.log('new diagram');
 
             this._diagram = diagram;
@@ -59,7 +59,7 @@ export class DisplayComponent implements OnDestroy {
     }
 
     private fetchFile(link: string) {
-        this._http.get(link,{
+        this._http.get(link, {
             responseType: 'text'
         }).pipe(
             catchError(err => {
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { APP_BASE_HREF, PlatformLocation } from '@angular/common';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import {APP_BASE_HREF, PlatformLocation} from '@angular/common';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {BrowserModule, bootstrapApplication} from '@angular/platform-browser';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AppComponent} from './app/app.component';
+import {importProvidersFrom} from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
